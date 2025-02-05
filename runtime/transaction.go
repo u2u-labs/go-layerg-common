a0c5b086a982c7a605df1f3607ac437706221312
MacOs <dangkimbao1999@gmail.com>
2025-02-05 16:38:15 +0700

    [REFACTOR] ua tx struct

--- runtime/transaction.go
+++ runtime/transaction.go
@@ -1,11 +1,5 @@
 package runtime
 
-import (
-	"math/big"
-
-	"github.com/ethereum/go-ethereum/common"
-)
-
 type ContractCallParams struct {
 	Sender          string
 	ContractAddress string
@@ -16,22 +10,42 @@ type ContractCallParams struct {
 }
 
 type TransactionRequest struct {
-	From  common.Address
-	To    common.Address
-	Data  string
-	Value *big.Int
+	Type                 *int          `json:"type,omitempty"`
+	To                   *string       `json:"to,omitempty"`
+	From                 *string       `json:"from,omitempty"`
+	Nonce                *int64        `json:"nonce,omitempty"`
+	GasLimit             *string       `json:"gasLimit,omitempty"`
+	GasPrice             *string       `json:"gasPrice,omitempty"`
+	MaxPriorityFeePerGas *string       `json:"maxPriorityFeePerGas,omitempty"`
+	MaxFeePerGas         *string       `json:"maxFeePerGas,omitempty"`
+	Data                 *string       `json:"data,omitempty"`
+	Value                *string       `json:"value,omitempty"`
+	ChainID              *string       `json:"chainId,omitempty"`
+	AccessList           *[]AccessItem `json:"accessList,omitempty"`
+	CustomData           interface{}   `json:"customData,omitempty"`
+	BlockTag             *string       `json:"blockTag,omitempty"`
+	EnableCcipRead       *bool         `json:"enableCcipRead,omitempty"`
+	BlobVersionedHashes  *[]string     `json:"blobVersionedHashes,omitempty"`
+	MaxFeePerBlobGas     *string       `json:"maxFeePerBlobGas,omitempty"`
+	Blobs                *[]string     `json:"blobs,omitempty"`
+	Kzg                  interface{}   `json:"kzg,omitempty"`
 }
 
-type OnchainTransactionPayload struct {
-	To                   string `json:"to"`
-	Value                string `json:"value"`
-	Data                 string `json:"data"`
-	MaxPriorityFeePerGas string `json:"maxPriorityFeePerGas"`
+type AccessItem struct {
+	Address     string   `json:"address"`
+	StorageKeys []string `json:"storageKeys"`
 }
 
-type OnchainTransactionRequest struct {
-	ProjectID      int                        `json:"projectId"`
-	ChainID        int                        `json:"chainId"`
-	Sponsor        bool                       `json:"sponsor"`
-	TransactionReq *OnchainTransactionPayload `json:"transactionReq"`
+// type OnchainTransactionPayload struct {
+// 	To                   string `json:"to"`
+// 	Value                string `json:"value"`
+// 	Data                 string `json:"data"`
+// 	MaxPriorityFeePerGas string `json:"maxPriorityFeePerGas"`
+// }
+
+type UATransactionRequest struct {
+	ProjectID      int                 `json:"projectId"`
+	ChainID        int                 `json:"chainId"`
+	Sponsor        bool                `json:"sponsor"`
+	TransactionReq *TransactionRequest `json:"transactionReq"`
 }
