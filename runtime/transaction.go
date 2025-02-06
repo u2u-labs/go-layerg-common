87dfb438a2b28f7e6623c75a99955d45da59d948
MacOs <dangkimbao1999@gmail.com>
2025-02-06 10:46:42 +0700

    minor

--- runtime/transaction.go
+++ runtime/transaction.go
@@ -48,3 +48,41 @@ type UATransactionRequest struct {
 	Sponsor        bool                `json:"sponsor"`
 	TransactionReq *TransactionRequest `json:"transactionReq"`
 }
+
+type UATransactionResponse struct {
+	Success bool   `json:"success"`
+	Data    Data   `json:"data"`
+	Message string `json:"message"`
+}
+
+type Data struct {
+	From       string `json:"from"`
+	To         string `json:"to"`
+	Amount     string `json:"amount"`
+	UserOpHash string `json:"userOpHash"`
+	User       User   `json:"user"`
+	CreatedAt  string `json:"createdAt"`
+	UpdatedAt  string `json:"updatedAt"`
+	ID         int    `json:"id"`
+}
+
+type User struct {
+	ID                int     `json:"id"`
+	CreatedAt         string  `json:"createdAt"`
+	UpdatedAt         string  `json:"updatedAt"`
+	Type              string  `json:"type"`
+	EoaWallet         string  `json:"eoaWallet"`
+	Username          *string `json:"username"`
+	Firstname         *string `json:"firstname"`
+	Lastname          *string `json:"lastname"`
+	Avatar            *string `json:"avatar"`
+	Signature         *string `json:"signature"`
+	PrimaryAAWallet   *string `json:"primaryAAWallet"`
+	TelegramID        string  `json:"telegramId"`
+	TelegramUsername  string  `json:"telegramUsername"`
+	TelegramFirstName string  `json:"telegramFirstName"`
+	TelegramLastName  string  `json:"telegramLastName"`
+	TelegramAvatarURL string  `json:"telegramAvatarUrl"`
+	TelegramAuthDate  string  `json:"telegramAuthDate"`
+	TelegramVerified  bool    `json:"telegramVerified"`
+}
