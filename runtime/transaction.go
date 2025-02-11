85765ca28ddbdae0f55e20f6481872173cc38cbd
MacOs <dangkimbao1999@gmail.com>
2025-02-11 15:14:18 +0700

    [UPDATE] ua send onchain res

--- runtime/transaction.go
+++ runtime/transaction.go
@@ -56,14 +56,45 @@ type UATransactionResponse struct {
 }
 
 type Data struct {
-	From       string `json:"from"`
-	To         string `json:"to"`
-	Amount     string `json:"amount"`
-	UserOpHash string `json:"userOpHash"`
-	User       User   `json:"user"`
-	CreatedAt  string `json:"createdAt"`
-	UpdatedAt  string `json:"updatedAt"`
-	ID         string `json:"id"`
+	From       string  `json:"from"`
+	To         string  `json:"to"`
+	Amount     string  `json:"amount"`
+	UserOpHash string  `json:"userOpHash"`
+	User       User    `json:"user"`
+	CreatedAt  string  `json:"createdAt"`
+	UpdatedAt  string  `json:"updatedAt"`
+	ID         string  `json:"id"`
+	Logs       []Log   `json:"logs"`
+	Receipt    Receipt `json:"receipt"`
+}
+
+type Log struct {
+	Address          string   `json:"address"`
+	Topics           []string `json:"topics"`
+	Data             string   `json:"data"`
+	BlockNumber      string   `json:"blockNumber"`
+	TransactionHash  string   `json:"transactionHash"`
+	TransactionIndex string   `json:"transactionIndex"`
+	BlockHash        string   `json:"blockHash"`
+	LogIndex         string   `json:"logIndex"`
+	Removed          bool     `json:"removed"`
+}
+
+type Receipt struct {
+	BlockHash         string  `json:"blockHash"`
+	BlockNumber       string  `json:"blockNumber"`
+	ContractAddress   *string `json:"contractAddress"`
+	CumulativeGasUsed string  `json:"cumulativeGasUsed"`
+	EffectiveGasPrice string  `json:"effectiveGasPrice"`
+	From              string  `json:"from"`
+	GasUsed           string  `json:"gasUsed"`
+	Logs              []Log   `json:"logs"`
+	LogsBloom         string  `json:"logsBloom"`
+	Status            string  `json:"status"`
+	To                string  `json:"to"`
+	TransactionHash   string  `json:"transactionHash"`
+	TransactionIndex  string  `json:"transactionIndex"`
+	Type              string  `json:"type"`
 }
 
 type User struct {
