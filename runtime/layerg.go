c959a2b4feaf1f8a6b02ea33fc9dacfc503f6139
t3201v <truong1320vv@gmail.com>
2025-02-28 11:48:46 +0700

    ft: twitter login

--- runtime/layerg.go
+++ runtime/layerg.go
@@ -19,6 +19,7 @@ type LayerGModule interface {
 	AuthenticateGameCenter(ctx context.Context, playerID, bundleID string, timestamp int64, salt, signature, publicKeyUrl, username string, create bool) (string, string, bool, error)
 	AuthenticateGoogle(ctx context.Context, token, username string, create bool, code, state, errStr string) (string, string, bool, error)
 	AuthenticateSteam(ctx context.Context, token, username string, create bool) (string, string, bool, error)
+	AuthenticateTwitter(ctx context.Context, username, code, state, errStr string, create bool) (string, string, bool, error)
 
 	AuthenticateTokenGenerate(userID, username string, exp int64, vars map[string]string) (string, int64, error)
 
