a2edb48d8c21c0f0122729ed77a4932e1b5b0d9d
MacOs <dangkimbao1999@gmail.com>
2025-07-04 14:42:50 +0700

    [UPDATE] peers

---
+++ runtime/peer.go
@@ -0,0 +1,18 @@
+package runtime
+
+import (
+	"context"
+
+	"github.com/u2u-labs/go-layerg-common/api"
+)
+
+type Peer interface {
+	// InvokeMS invoke microservice
+	InvokeMS(ctx context.Context, in *api.AnyRequest) (*api.AnyResponseWriter, error)
+
+	// Send
+	SendMS(ctx context.Context, in *api.AnyRequest) error
+
+	//Event
+	Event(ctx context.Context, in *api.AnyRequest, names ...string) error
+}
